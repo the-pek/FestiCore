ad6ed6cecf82816a637eb17c0aafddc64c73c595
Alexandre BILLOTTE <billotte@et.esiea.fr>
2026-01-13 09:09:59 +0100

    Add

---
+++ src/test/java/org/esiea/festicore/UserTest.java
@@ -0,0 +1,13 @@
+package org.esiea.festicore;
+
+import org.esiea.festicore.Enumeration.TicketType;
+import java.time.LocalDate;
+
+import org.esiea.festicore.Exceptions.ReservationException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+    
+}
